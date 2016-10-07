b943c4a2846ec303a5032252590b96992ddaaeaa
Jake Renshaw <Jake.Renshaw@Gamesys.co.uk>
2016-10-07 16:22:38 +0100

    Re-skin on rocky bike to rocky stairs

--- stairsgame.js
+++ stairsgame.js
@@ -1,16 +1,23 @@
 var canvas = document.getElementById('myCanvas'),
 context = canvas.getContext('2d');
-background = new Image();
-rockyBike = new Image();
+stairs = new Image();
+rockyRun = new Image();
 win = new Image();
 lose = new Image();
+rockyRuns = new Image();
 
-background.src = 'street.jpg';
-rockyBike.src = 'rockybike.png';
+stairs.src = 'stairs.jpg';
+rockyRun.src = 'rockyRun.png';
+rockyRuns.src = 'rockyRuns.png';
 win.src = 'win.jpg';
 lose.src = 'lose.jpg';
 
-var bikePosition = -50;
+var stepsV = 150;
+var stepsX = 50;
+var sizeW = 400;
+var sizeH = 400;
+var flip = 1;
+
 var bet;
 var timer = 10;
 var distance = 0;
@@ -22,9 +29,9 @@ var loseMusic = new Audio('losecut.mp3');
 startGame();
 
 function startGame(){
-background.onload = function() {
-	context.drawImage(background, 0, 0, 900, 500);
-	context.drawImage(rockyBike, bikePosition, 180, 400, 400);
+stairs.onload = function() {
+	context.drawImage(stairs, 0, 0, 900, 500);
+	context.drawImage(rockyRun, stepsX, stepsV, sizeW, sizeH);
 };
 }
 
@@ -34,20 +41,31 @@ function placeBet(n){
 }
 
 function playGame(){
-	context.drawImage(background, 0, 0, 900, 500);
+	context.drawImage(stairs, 0, 0, 900, 500);
 	context.font="50px Georgia";
 	context.fillText("GO!", 400, 100);
-	context.drawImage(rockyBike, bikePosition, 180, 400, 400);
+	context.drawImage(rockyRun, stepsX, stepsV, sizeW, sizeH);
 	setTimeout(function(){ endGame() }, 10000);
 	music.play();
 	document.body.onkeyup = function(e){
 		if(e.keyCode == 32){
-			bikePosition += 5;
+			stepsV -= 0.5;
+			stepsX += 4;
+			sizeH -= 4;
+			sizeW -= 4;
 			distance += 0.5;
-			context.drawImage(background, 0, 0, 900, 500);
+			flip += 1;
+			context.drawImage(stairs, 0, 0, 900, 500);
 			context.font="30px Georgia";
 			context.fillText(distance + "m", 800, 50);
-			context.drawImage(rockyBike, bikePosition, 180, 400, 400);
+			if (flip % 2 === 0)
+			{
+				context.drawImage(rockyRun, stepsX, stepsV, sizeW, sizeH);
+			}
+			else
+			{
+				context.drawImage(rockyRuns, stepsX, stepsV, sizeW, sizeH);
+			}
 		}
 	}
 }
@@ -82,3 +100,4 @@ function endGame(){
 		context.fillText(loseMes, 170, 30);
 	}
 }
+
